df1266b2a442c02496af030770b1edcc8c4ecef7
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2022-01-05 20:12:29 +0100

    Front initialisation

---
+++ neo-back/controllers/upload.controller.js
@@ -0,0 +1,6 @@
+const Usermodel = require("../models/user.model");
+const fs = require("fs");
+const { promisify } = require("util");
+const pipeline = promisify(require("stream").pipeline);
+
+module.exports.uploadProfil = async (req, res) => {};
