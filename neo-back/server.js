d76f687952b9c0dff332fb08dc86dc77823b52da
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2022-01-06 10:16:45 +0100

    Back v1

--- neo-back/server.js
+++ neo-back/server.js
@@ -9,7 +9,20 @@ require("./config/db");
 
 const { checkUser, requireAuth } = require("./middlewares/auth.middleware");
 
+const cors = require("cors");
+
 const app = express();
+
+const corsOptions = {
+	origin: process.env.CLIENT_URL,
+	credentials: true,
+	allowedHeaders: ["sessionId", "Content-Type"],
+	exposedHeaders: ["sessionId"],
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	preflightContinue: false,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
